40c10a4ffbc1445ff3d4dfa602de1bec2dd24a6f
pojanicha <pojanicha.sora@bumail.net>
2026-09-17 16:50:24 +0700

    Assignment (finished)

--- Assets/Scripts/Workspace/Assignment/StudentSolution.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
+using System;
 
 namespace Assignment
 {
@@ -114,21 +115,121 @@ namespace Assignment
 
         public int[] AS01_SelectionSortDescending(int[] numbers)
         {
+            int n = numbers.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int maxIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (numbers[j] > numbers[maxIndex])
+                    {
+                        maxIndex = j;
+                    }
+                }
+                (numbers[i], numbers[maxIndex]) = (numbers[maxIndex], numbers[i]);
+            }
+
+            foreach (var _n in numbers)
+            { 
+
+                Debug.Log(_n);
+
+
+            }
+
+
             return numbers;
         }
 
         public int[] AS02_BubbleSortDescending(int[] numbers)
         {
+            int n = numbers.Length;
+
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (numbers[j] < numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+
+
+            foreach (var _n in numbers)
+            {
+                Debug.Log(_n);
+            }
+
+
             return numbers;
         }
 
         public int[] AS03_InsertionSortDescending(int[] numbers)
         {
+
+            int n = numbers.Length;
+
+            for (int i = 1; i < n; i++)
+            {
+                int key = numbers[i];
+                int j = i - 1;
+                
+                while (j >= 0 && numbers[j] < key)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+                numbers[j + 1] = key;
+            }
+
+            foreach (var _n in numbers)
+            {
+                Debug.Log(_n);
+            }
+
             return numbers;
         }
 
         public int AS04_FindTheSecondLargestNumber(int[] numbers)
         {
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+
+            int largest = numbers[0];
+            int secondLargest = int.MinValue;
+            bool found = false;
+
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < largest)
+                {
+                    secondLargest = numbers[i];
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found)
+            { 
+                Debug.Log(secondLargest);
+                return secondLargest;
+            }
+            else
+            {
+                Debug.Log("No second largest number found.");
+                return int.MinValue;
+
+
+            }
+
+
             return 0;
         }
 
